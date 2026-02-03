b42210df89297dadf2615aa0027df5403d2fa7f0
Your Name <laura.kotlinska.96@gmail.com>
2026-02-03 12:38:43 +0100

    Boilerplate

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+mongoose.connect(process.env.DB_CONNECTION).then(
+  console.log("Connected to DB."),
+  app.listen(port, () => {
+    console.log(`API running on port ${port}`);
+  }),
+);
