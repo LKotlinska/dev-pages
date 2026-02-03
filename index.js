bf2f6eb6b23f5ae8fa96390f94f26943a71c71dd
Your Name <laura.kotlinska.96@gmail.com>
2026-02-03 17:06:49 +0100

    Render registration form. Submission to database.

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
-import router from "./routes/auth.js";
-import authRouter from "./routes/auth.js";
+import authRouter from "./routes/register.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// App Configuration
 
 const app = express();
-const port = process.env.PORT;
-// const db = mongoose.connection;
+const port = process.env.PORT || 8000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/register", authRouter);
 
-import profileRouter from './routes/profiles.js'
+import profileRouter from "./routes/profiles.js";
 
-app.use('/profiles', profileRouter);
+app.use("/profiles", profileRouter);
+
+app.get("/register", (req, res) => {
+  res.render("register", { test: "hehe" });
+});
 
 mongoose.connect(process.env.DB_CONNECTION).then(() => {
   console.log("Connected to DB.");
